8d268d85735a5af4519cce4ff66e72573b3de727
pgm-ryanjudd <ryanjudd@student.artveldehs.be>
2020-12-02 16:30:17 +0100

    folder structure update

---
+++ _static/js/data.js
@@ -0,0 +1,20 @@
+const webshop = [
+  {
+    name: "Boeket small",
+    type: "small",
+    img: "_static/images/small-boeket.png",
+    price: "30",
+  },
+  {
+    name: "Boeket medium",
+    type: "medium",
+    img: "_static/images/medium-boeket.png",
+    price: "40",
+  },
+  {
+    name: "Boeket large",
+    type: "large",
+    img: "_static/images/large-boeket.png",
+    price: "50",
+  },
+]
--- _static/js/main.js
+++ _static/js/main.js
@@ -7,6 +7,8 @@
       this.onClickList();
       this.onClickOpenBasket();
       this.onClickCloseBasket();
+      this.fillWebshop();
+      this.webshopType();
     },
     cacheElements() {
       this.$burger = document.querySelector(".svg__burger");
@@ -14,6 +16,8 @@
       this.$basket = document.querySelector(".basket");
       this.$basketAside = document.querySelector(".basket-aside");
       this.$cross = document.querySelector('.svg__cross');
+      this.$webshop = document.querySelector("#webshopLoad");
+      this.$webshopContainer = document.querySelector("#typeLoader");
     },
     onClickList() {
       this.$burger.addEventListener("click", () => {
@@ -41,6 +45,77 @@
 
         this.$basketAside.classList.remove('fill-basket');
       });
+    },
+
+    fillWebshop() {
+      console.log('Webshop data loading Working');
+
+      let webshopStr = "";
+
+      if (this.$webshop !== null) {
+        
+        webshop.forEach((webshopItem) => {
+//           webshopStr += `<div class="webshopFlex">
+// <img src="${webshopItem.img}" alt="${webshopItem.type} groot boeket">
+// <div class="webshopDynamic">
+// <a href="boeket.html?type=${webshopItem.type}"> ${webshopItem.name}</a>
+// <p>€ ${webshopItem.price}</p>
+// </div>
+// </div>
+//          `
+
+
+          webshopStr += ` <div class="webshopFlex">
+          <figure>
+            <a href="boeket.html?type=${webshopItem.type}">
+              <img
+                src="${webshopItem.img}"
+                alt="een ${webshopItem.type} boeket"
+              />
+              <figcaption>Boeket ${webshopItem.type}</figcaption>
+            </a>
+            <span>€ ${webshopItem.price}</span>
+          </figure>
+          </div>
+          `
+        });
+
+        this.$webshop.innerHTML = webshopStr;
+      }
+    },
+
+    webshopType() {
+      const search = window.location.search;
+      const params = new URLSearchParams(search);
+      const urlType = params.get('type');
+
+      if (urlType !== null) {
+        console.log("Type loaing works");
+
+        let typeItem = "";
+        let typeStr = "";
+
+        for (let i = 0; i < webshop.length; i++) {
+          if (webshop[i].type === urlType) {
+            typeItem = webshop[i];
+          }
+        };
+        
+        if (typeItem) {
+          typeStr = `
+<img src="${typeItem.img}" alt="${typeItem.name}">
+<div class="webshop-type-top">
+<h1>${typeItem.name}</h1>
+<p class="Webshop-type-price">€ ${typeItem.price}</p>
+</div>          
+          `
+          window.document.title = typeItem.name;
+          this.$webshopContainer.innerHTML = typeStr;
+        } else {
+          window.document.title = "Niet gevonden";
+        }
+
+      }
     }
     
   };
